b46782ba8dc84c6e658e764cc31635bb98253c4b
안나광 <ahnnagwang@naver.com>
2022-09-27 22:42:55 +0900

    자바스크립트 공부
    산술연산자

---
+++ plus.js
@@ -0,0 +1,12 @@
+var value = 1+2;
+console.log(value);
+
+// 왼쪽에서 오른쪽
+var value = 1+5+"abc";
+console.log(value);
+
+// 숫자로 변환
+// 1또는0으로변환
+console.log(10 + null);
+console.log(10 + true);
+console.log(10 + false);
