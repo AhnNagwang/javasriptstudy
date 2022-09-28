81daff3f8b98c4b35a30b73befbb43a975621642
안나광 <ahnnagwang@naver.com>
2022-09-28 23:19:48 +0900

    자바스크립트 공부
    if,dowhile 공부

---
+++ ifdowhile.js
@@ -0,0 +1,30 @@
+// if 문
+var a = 1 , b = 2;
+if(a===b) {
+  console.log("블록사용");
+};
+
+// if else문
+
+var a = 1 , b = 2;
+if(a===b){
+  log("블록사용하지않음");
+}else {
+  log("블록사용하지않음");
+};
+
+// while 문
+var k=1;
+while(k<0){
+  console.log(k);
+  k++;
+};
+
+// dowhile
+var k=0;
+do {
+  console.log("do:",k);
+  k++;
+}while(k<3) {
+  console.log("while:",k);
+};
